15df40ed020c9f726fb4d1c571c2463802d731be
Antonio Stoilkov <antonio.stoilkov@gmail.com>
2020-03-15 14:25:58 +0200

    :white_check_mark: raise test coverage to 100%

--- test.ts
+++ test.ts
@@ -32,6 +32,19 @@ describe('useLocalStorageState()', () => {
         expect(todos).toEqual(['third', 'forth'])
     })
 
+    it('updates state with callback function', () => {
+        const { result } = renderHook(() => useLocalStorageState('todos', ['first', 'second']))
+
+        act(() => {
+            const setTodos = result.current[1]
+
+            setTodos(value => [...value, 'third', 'forth'])
+        })
+
+        const [todos] = result.current
+        expect(todos).toEqual(['first', 'second', 'third', 'forth'])
+    })
+
     it('updating writes into localStorage', () => {
         const { result } = renderHook(() => useLocalStorageState('todos', ['first', 'second']))
 
@@ -69,51 +82,68 @@ describe('useLocalStorageState()', () => {
         const [todosB] = resultB.current
         expect(todosB).toEqual(['third', 'forth'])
     })
-})
 
-describe('createLocalStorageStateHook()', () => {
-    it('initial state is written into the state', () => {
-        const useTodos = createLocalStorageStateHook('todos', ['first', 'second'])
-        const { result } = renderHook(() => useTodos())
-
-        const [todos] = result.current
-        expect(todos).toEqual(['first', 'second'])
-    })
-
-    it(`initial state isn't written into localStorage`, () => {
-        const useTodos = createLocalStorageStateHook('todos', ['first', 'second'])
-        renderHook(() => useTodos())
-
-        expect(localStorage.getItem('todos')).toEqual(null)
-    })
-
-    it('updates state', () => {
-        const useTodos = createLocalStorageStateHook('todos', ['first', 'second'])
-        const { result } = renderHook(() => useTodos())
+    it('storage event updates state to default value', () => {
+        const { result } = renderHook(() => useLocalStorageState('todos', ['first', 'second']))
 
         act(() => {
             const setTodos = result.current[1]
-
             setTodos(['third', 'forth'])
+
+            window.dispatchEvent(
+                new StorageEvent('storage', {
+                    storageArea: localStorage,
+                    key: 'todos',
+                    oldValue: JSON.stringify(['first', 'second']),
+                    newValue: null,
+                }),
+            )
         })
+
+        const [todosB] = result.current
+        expect(todosB).toEqual(['first', 'second'])
+    })
+
+    it(`unrelated storage update doesn't do anything`, () => {
+        const { result } = renderHook(() => useLocalStorageState('todos', ['first', 'second']))
+
+        act(() => {
+            // trying with sessionStorage
+            window.dispatchEvent(
+                new StorageEvent('storage', {
+                    storageArea: sessionStorage,
+                    key: 'todos',
+                    oldValue: JSON.stringify(['first', 'second']),
+                    newValue: JSON.stringify(['third', 'forth']),
+                }),
+            )
+
+            // trying with a non relevant key
+            window.dispatchEvent(
+                new StorageEvent('storage', {
+                    storageArea: localStorage,
+                    key: 'not-todos',
+                    oldValue: JSON.stringify(['first', 'second']),
+                    newValue: JSON.stringify(['third', 'forth']),
+                }),
+            )
+        })
+
+        const [todosA] = result.current
+        expect(todosA).toEqual(['first', 'second'])
+    })
+
+    it('initially gets value from local storage if there is a value', () => {
+        localStorage.setItem('todos', JSON.stringify(['third', 'forth']))
+
+        const { result } = renderHook(() => useLocalStorageState('todos', ['first', 'second']))
 
         const [todos] = result.current
         expect(todos).toEqual(['third', 'forth'])
     })
+})
 
-    it('updating writes into localStorage', () => {
-        const useTodos = createLocalStorageStateHook('todos', ['first', 'second'])
-        const { result } = renderHook(() => useTodos())
-
-        act(() => {
-            const setTodos = result.current[1]
-
-            setTodos(['third', 'forth'])
-        })
-
-        expect(localStorage.getItem('todos')).toEqual(JSON.stringify(['third', 'forth']))
-    })
-
+describe('createLocalStorageStateHook()', () => {
     it('storage event updates state', () => {
         const useTodos = createLocalStorageStateHook('todos', ['first', 'second'])
         const { result: resultA } = renderHook(() => useTodos())
