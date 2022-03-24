83dd917a93c8d6f54e236ab93c9ecf10e32665a6
Antonio Stoilkov <hello@astoilkov.com>
2022-03-24 20:26:10 +0200

    ✅ should not call useLayoutEffect() on the server

--- test.tsx
+++ test.tsx
@@ -565,6 +565,25 @@ describe('createLocalStorageStateHook()', () => {
             })
         })
 
+        it(`should not call useLayoutEffect() on the server`, () => {
+            // When you call `useLayoutEffect()` on the server React calls `console.error` with the
+            // following message:
+            // "Warning: useLayoutEffect does nothing on the server, because its effect cannot be
+            // encoded into the server renderer's output format. This will lead to a mismatch between
+            // the initial, non-hydrated UI and the intended UI. To avoid this, useLayoutEffect should
+            // only be used in components that render exclusively on the client. See
+            // https://reactjs.org/link/uselayouteffect-ssr for common fixes."
+            const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+
+            renderHookOnServer(() =>
+                useLocalStorageState('number', {
+                    defaultValue: 0,
+                }),
+            )
+
+            expect(errorSpy).not.toHaveBeenCalled()
+        })
+
         it('returns default value on the server', () => {
             localStorage.setItem('todos', JSON.stringify(['third', 'forth']))
 
