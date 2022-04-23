49e40543146a5ce836791341cabd880213f7cf2a
Antonio Stoilkov <hello@astoilkov.com>
2022-04-23 09:39:15 +0300

    🎨 run prettier

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,11 +4,7 @@ module.exports = {
     // what about: ts-node CLI option:
     // --prefer-ts-exts Re-order file extensions so that TypeScript imports are preferred (TS_NODE_PREFER_TS_EXTS, default: false)
     moduleNameMapper: {
-        '^(\\..*)$': [
-            '$1/index.ts',
-            '$1.ts',
-            '$1'
-        ]
+        '^(\\..*)$': ['$1/index.ts', '$1.ts', '$1'],
     },
     testMatch: ['<rootDir>/test.tsx'],
 }
