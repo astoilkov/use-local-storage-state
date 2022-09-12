b3cfa46927a27c282685a6790fd5400a9b48c64f
Antonio Stoilkov <hello@astoilkov.com>
2022-09-12 13:29:14 +0300

    🏗 simpler condition + less bundle size

--- src/useLocalStorageState.ts
+++ src/useLocalStorageState.ts
@@ -41,7 +41,7 @@ export default function useLocalStorageState<T = undefined>(
     options?: LocalStorageOptions<T | undefined>,
 ): LocalStorageState<T | undefined> {
     // istanbul ignore next
-    if (typeof useSyncExternalStore === 'undefined') {
+    if (useSyncExternalStore === undefined) {
         throw new TypeError(
             `You are using React 17 or below. Install with "npm install use-local-storage-state@17".`,
         )
