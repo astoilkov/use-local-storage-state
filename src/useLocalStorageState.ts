87a529297c73c4fc33e0406297b6abf3dde8a2dd
Antonio Stoilkov <hello@astoilkov.com>
2024-07-08 14:36:15 +0300

    🐜 reverse order

--- src/useLocalStorageState.ts
+++ src/useLocalStorageState.ts
@@ -106,7 +106,7 @@ function useLocalStorage<T>(
             //   issues that were caused by incorrect initial and secondary implementations:
             //   - https://github.com/astoilkov/use-local-storage-state/issues/30
             //   - https://github.com/astoilkov/use-local-storage-state/issues/33
-            if (string === null && defaultValue !== undefined) {
+            if (defaultValue !== undefined && string === null) {
                 // reasons for `localStorage` to throw an error:
                 // - maximum quota is exceeded
                 // - under Mobile Safari (since iOS 5) when the user enters private mode
